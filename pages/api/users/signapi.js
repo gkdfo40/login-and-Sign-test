18ca22ec3ca0ea606076852d974144ed39bbfa3d
gkdfo40 <gkdfo40@gmail.com>
2022-02-08 20:51:59 +0900

    Fix sign API

--- pages/api/users/signapi.js
+++ pages/api/users/signapi.js
@@ -9,10 +9,14 @@ export default async function handler(req, res) {
   switch (method) {
     case 'POST':
       try {
-        const user = await User.create(req.body);
-        res.status(201).json({ success: true, data: user })
+        const data = await User.find({ username: req.body.username })
+        if (!!data) {
+          await User.create(req.body);
+          res.status(201).end()
+        }
+        else res.status(400).end()
       } catch (error) {
-        res.status(400).json({ success: false })
+        res.status(400).end()
       }
       break
     case 'GET':
